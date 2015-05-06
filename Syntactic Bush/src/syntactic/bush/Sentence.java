d818e968410a0328761d4fd8c99417fd8876a0b7
spriadka <manik949@gmail.com>
2015-05-06 17:45:46 +0200

    Skusame metodu readSyntax

--- Syntactic Bush/src/syntactic/bush/Sentence.java
+++ Syntactic Bush/src/syntactic/bush/Sentence.java
@@ -5,6 +5,13 @@
  */
 package syntactic.bush;
 
+import java.io.File;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
 /**
  *
  * @author omacejovsky
@@ -61,6 +68,41 @@ public class Sentence {
      */
     public void setSentence(String[] sentence) {
         this.sentence = sentence;
+    }
+    
+    public void readSyntax(){
+        try {
+            File file = new File("." + File.separator + "src" + File.separator + "ff.syntax.xml");
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(file);
+            System.out.println("Root element " + doc.getDocumentElement().getNodeName());
+            NodeList syntaxes = doc.getElementsByTagName("syntax nite:");
+            for (int i=0; i < syntaxes.getLength(); i++){
+                Element syntax = (Element)syntaxes.item(i);
+                if (Integer.parseInt(syntax.getAttribute("status")) == 1){
+                    Element child = (Element)syntax.getElementsByTagName("nite:child").item(0);
+                    String subString = "ff.text.[0-9]*"; //substring of the atribute href
+                    int from,to;
+                    String href = child.getAttribute("href");
+                    int fromIndex = href.indexOf(subString); //indexOf(String str) returns first index of the string when str occurs
+                    int toIndex = href.lastIndexOf(subString); //lastIndexOf returns last index of the string when str occurs
+                    
+                }
+                
+            }
+            
+            NodeList tokens = sentence.getElementsByTagName("token");
+            for (int i=0; i < tokens.getLength(); i++ ){
+                System.out.println(tokens.item(i).getTextContent());
+            }
+            
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
     }
    
     
