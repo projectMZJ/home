eb6f4244328b5dad121b4b4427fa3781858752c8
spriadka <manik949@gmail.com>
2015-05-06 17:24:17 +0200

    Revert "pridana trida sentence a zacatek parsovni text.xml"
    This reverts commit 0f5a17545dfb3987632552315fb2a7c7d3fb6c77.

--- Syntactic Bush/src/syntactic/bush/SyntacticBush.java
+++ Syntactic Bush/src/syntactic/bush/SyntacticBush.java
@@ -7,14 +7,13 @@ package syntactic.bush;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.util.ArrayList;
-import java.util.List;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.apache.commons.io.*;
 
 /**
  *
@@ -25,12 +24,7 @@ public class SyntacticBush {
     /**
      * @param args the command line arguments
      */
-
-    
-    
     public static void main(String[] args) {
-        int head,tail;
-        List<String> phrases = new ArrayList<>();
         try {
             File file = new File("." + File.separator + "src" + File.separator + "ff.text.xml");
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
@@ -39,25 +33,16 @@ public class SyntacticBush {
             System.out.println("Root element " + doc.getDocumentElement().getNodeName());
             NodeList sentences = doc.getElementsByTagName("s");
             Element sentence = (Element)sentences.item(0);
-            head = lastIndex(sentence.getAttribute("nite:id"));
             NodeList tokens = sentence.getElementsByTagName("token");
             for (int i=0; i < tokens.getLength(); i++ ){
-                Element token = (Element) tokens.item(i);
-                phrases.add(Integer.toString(lastIndex(token.getAttribute("nite:id"))) + tokens.item(i).getTextContent());
+                System.out.println(tokens.item(i).getTextContent());
             }
-            tail = head + tokens.getLength();
-            Sentence s = new Sentence(tail,head, phrases.toArray(new String[phrases.size()]));
-            System.out.println(s);
-            //zavolani konstruktoru Sentence(head,tail,phrase)
+            
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
 
-    }
-    
-    public static int lastIndex(String str){
-        return Integer.parseInt(str.split("\\.")[2]);
     }
 
 }
