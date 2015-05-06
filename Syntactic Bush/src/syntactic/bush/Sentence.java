87a73c033a39716300d9c499dd285f3f64f67736
omacejovsky <omacejovsky@OMACEJOVSKY.clance.local>
2015-05-06 11:25:53 +0200

    pridana trida sentence a zacatek parsovni text.xml

---
+++ Syntactic Bush/src/syntactic/bush/Sentence.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package syntactic.bush;
+
+/**
+ *
+ * @author omacejovsky
+ */
+public class Sentence {
+    private Integer tail;   
+    private Integer head;
+    private String[] sentence;
+    
+    public Sentence(Integer tail, Integer head, String[] sentence)
+    {
+        this.tail = tail;
+        this.head = head;
+        this.sentence = sentence;
+    }
+
+    /**
+     * @return the tail
+     */
+    public Integer getTail() {
+        return tail;
+    }
+
+    /**
+     * @param tail the tail to set
+     */
+    public void setTail(Integer tail) {
+        this.tail = tail;
+    }
+
+    /**
+     * @return the head
+     */
+    public Integer getHead() {
+        return head;
+    }
+
+    /**
+     * @param head the head to set
+     */
+    public void setHead(Integer head) {
+        this.head = head;
+    }
+
+    /**
+     * @return the sentence
+     */
+    public String[] getSentence() {
+        return sentence;
+    }
+
+    /**
+     * @param sentence the sentence to set
+     */
+    public void setSentence(String[] sentence) {
+        this.sentence = sentence;
+    }
+   
+    
+}
--- Syntactic Bush/src/syntactic/bush/SyntacticBush.java
+++ Syntactic Bush/src/syntactic/bush/SyntacticBush.java
@@ -5,6 +5,14 @@
  */
 package syntactic.bush;
 
+import java.io.File;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 /**
  *
  * @author omacejovsky
@@ -15,7 +23,19 @@ public class SyntacticBush {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            File file = new File(".\\src\\ff.text.xml");
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(file);
+            System.out.println("Root element " + doc.getDocumentElement().getNodeName());
+            NodeList sentences = doc.getElementsByTagName("s");
+            
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
-    
+
 }
