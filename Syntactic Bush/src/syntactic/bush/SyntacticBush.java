78f1582a15db06f88cb085f439f39fbfb63f32a4
spriadka <manik949@gmail.com>
2015-05-06 11:41:15 +0200

    Parse text.txt working

--- Syntactic Bush/src/syntactic/bush/SyntacticBush.java
+++ Syntactic Bush/src/syntactic/bush/SyntacticBush.java
@@ -6,12 +6,14 @@
 package syntactic.bush;
 
 import java.io.File;
+import java.io.FileInputStream;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.apache.commons.io.*;
 
 /**
  *
@@ -24,12 +26,17 @@ public class SyntacticBush {
      */
     public static void main(String[] args) {
         try {
-            File file = new File(".\\src\\ff.text.xml");
+            File file = new File("." + File.separator + "src" + File.separator + "ff.text.xml");
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             DocumentBuilder db = dbf.newDocumentBuilder();
             Document doc = db.parse(file);
             System.out.println("Root element " + doc.getDocumentElement().getNodeName());
             NodeList sentences = doc.getElementsByTagName("s");
+            Element sentence = (Element)sentences.item(0);
+            NodeList tokens = sentence.getElementsByTagName("token");
+            for (int i=0; i < tokens.getLength(); i++ ){
+                System.out.println(tokens.item(i).getTextContent());
+            }
             
             
         } catch (Exception e) {
