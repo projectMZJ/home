581b71eb25352e52e0df63798ef0abe31b68a80b
Peta <Peta@PC-Peta>
2015-05-06 21:08:12 +0200

    Add documentation, change and finish readSyntax(), add new atribut idSentence

--- Syntactic Bush/src/syntactic/bush/Sentence.java
+++ Syntactic Bush/src/syntactic/bush/Sentence.java
@@ -11,21 +11,25 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
  * @author omacejovsky
  */
 public class Sentence {
-    private Integer tail;   
-    private Integer head;
-    private String[] sentence;
+    private Integer tail; //index konce vety 
+    private Integer head; // index zacatku vety
+    private String[] sentence; //veta ulozena v poli po slovech
+    private final int[] idSentence; //k sentence, pole kazdemu slovu v poli sentence prideluje jeho cislo v ff.text.xml
     
-    public Sentence(Integer tail, Integer head, String[] sentence)
+    public Sentence(Integer tail, Integer head, int[] idSentence, String[] sentence)
     {
         this.tail = tail;
         this.head = head;
         this.sentence = sentence;
+        this.idSentence = idSentence;
     }
 
     /**
@@ -70,40 +74,88 @@ public class Sentence {
         this.sentence = sentence;
     }
     
+    /**
+     * toString return array in one string
+     * @return one strinf full of parts of array
+     */
+    public String toString(){
+        String result = "";
+        for (int i=0; i < sentence.length; i++){
+            result += sentence[i];
+            result += " ";
+        }
+        return result;
+    }
+    
+    /**
+     * readSyntax() read syntax
+     */
     public void readSyntax(){
         try {
             File file = new File("." + File.separator + "src" + File.separator + "ff.syntax.xml");
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             DocumentBuilder db = dbf.newDocumentBuilder();
             Document doc = db.parse(file);
-            System.out.println("Root element " + doc.getDocumentElement().getNodeName());
-            NodeList syntaxes = doc.getElementsByTagName("syntax nite:");
-            for (int i=0; i < syntaxes.getLength(); i++){
+            NodeList syntaxes = doc.getElementsByTagName("syntax");
+            for (int i=0; i < 15/*syntaxes.getLength()*/; i++){
                 Element syntax = (Element)syntaxes.item(i);
-                if (Integer.parseInt(syntax.getAttribute("status")) == 1){
-                    Element child = (Element)syntax.getElementsByTagName("nite:child").item(0);
-                    String subString = "ff.text.[0-9]*"; //substring of the atribute href
-                    int from,to;
-                    String href = child.getAttribute("href");
-                    int fromIndex = href.indexOf(subString); //indexOf(String str) returns first index of the string when str occurs
-                    int toIndex = href.lastIndexOf(subString); //lastIndexOf returns last index of the string when str occurs
-                    
-                }
                 
+                //zabyvat se jenom temi co maji status 1
+                if (Integer.parseInt(syntax.getAttribute("status")) == 1){
+                    Integer indicator = lastIndex(syntax.getAttribute("nite:id")); //hodnosta ff.syntax.x
+                    Element child = (Element)syntax.getElementsByTagName("nite:child").item(0);
+                    String href = child.getAttribute("href");
+                    
+                    //parser href
+                    Pattern pattern = Pattern.compile("[0-9]+");
+                    Matcher matcher;
+                    matcher = pattern.matcher(href);
+                    int from = 0; //zacatek skupiny
+                    int to = 0; //konec skupiny
+                    if (matcher.find()) {
+                        from = Integer.parseInt(matcher.group(0));
+                        if(matcher.find()) to = Integer.parseInt(matcher.group(0));
+                        else to = from;
+                    } 
+                    
+                    //for skonci kdyz je from vetsi nez tail, tzn. uz jsme u dalsi vety
+                    if(from > this.tail) break;
+                    
+                    //slouceni skupin do poli k sobe
+                    int indexTo = 0;
+                    int indexFrom = 0;
+                    if(from != to){
+                        int j;
+                        
+                        //nalezeni, kde indexu kde zacina a konci cast skupiny v sentece
+                        for(j = 0; j < tail-head; j++){
+                            if(idSentence[j] == to) indexTo = j;
+                            if(idSentence[j] == from) indexFrom = j;
+                        }
+                        
+                        //ulozeni vsech podcasti do jedne
+                        while(indexFrom != indexTo){
+                            sentence[indexTo-1] = sentence[indexTo-1] + " " + sentence[indexTo];
+                            sentence[indexTo] = null;
+                            indexTo--;
+                        }           
+                    }
+                    
+                    //kvuli vypisu
+                    int j;
+                    for(j = 0; j < tail-head; j++){
+                            if(idSentence[j] == to) indexTo = j;
+                            if(idSentence[j] == from) indexFrom = j;
+                    }
+                    System.out.println(sentence[indexFrom]); 
+                }
             }
-            
-            NodeList tokens = sentence.getElementsByTagName("token");
-            for (int i=0; i < tokens.getLength(); i++ ){
-                System.out.println(tokens.item(i).getTextContent());
-            }
-            
-            
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
     }
    
-    
+    public static int lastIndex(String str){
+        return Integer.parseInt(str.split("\\.")[2]);
+    }
 }
--- Syntactic Bush/src/syntactic/bush/SyntacticBush.java
+++ Syntactic Bush/src/syntactic/bush/SyntacticBush.java
@@ -7,13 +7,14 @@ package syntactic.bush;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.apache.commons.io.*;
 
 /**
  *
@@ -23,26 +24,42 @@ public class SyntacticBush {
 
     /**
      * @param args the command line arguments
-     */
+     */   
     public static void main(String[] args) {
+        int head,tail; //zacatek a konec vety
+        List<String> phrases = new ArrayList<>(); //list slov ve vete
+        int[] idSentence; //pole id z ff.text
         try {
             File file = new File("." + File.separator + "src" + File.separator + "ff.text.xml");
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             DocumentBuilder db = dbf.newDocumentBuilder();
             Document doc = db.parse(file);
-            System.out.println("Root element " + doc.getDocumentElement().getNodeName());
             NodeList sentences = doc.getElementsByTagName("s");
             Element sentence = (Element)sentences.item(0);
+            head = lastIndex(sentence.getAttribute("nite:id"));
             NodeList tokens = sentence.getElementsByTagName("token");
+            idSentence = new int[tokens.getLength()];
             for (int i=0; i < tokens.getLength(); i++ ){
-                System.out.println(tokens.item(i).getTextContent());
+                Element token = (Element) tokens.item(i);
+                idSentence[i] = lastIndex(token.getAttribute("nite:id"));
+                phrases.add(/*Integer.toString(lastIndex(token.getAttribute("nite:id"))) +*/ tokens.item(i).getTextContent());
             }
-            
+            tail = head + tokens.getLength();
+            Sentence s = new Sentence(tail,head, idSentence, phrases.toArray(new String[phrases.size()]));
+            System.out.println(s);
+            s.readSyntax();
             
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
-
+    
+    /**
+     * Function for split str and return its third part converted to int
+     * @param str string to split
+     * @return third part of ff.text.x
+     */
+    public static int lastIndex(String str){
+        return Integer.parseInt(str.split("\\.")[2]);
+    }
 }
