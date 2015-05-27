128614cb89ab8e63a31e21f683aa601da16e778c
fprazak <prazak.filip@seznam.cz>
2015-05-27 01:20:00 +0000

    pridani pst + maven

---
+++ syntatic bush old/src/syntactic/bush/ColorConverter.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package syntactic.bush;
+
+/**
+ *
+ * @author Šimon Priadka
+ */
+public class ColorConverter {
+    
+    
+    /**
+     * Returns hexadecimal representation for given colour in hexadecimal format, that is gonna be used firther in repersentation of the data
+     * 
+     * @param cyan
+     * @param magenta
+     * @param yellow
+     * @param black
+     * @return 
+     */
+    public static String cmykToHex(float cyan, float magenta, float yellow, float black) {
+        int r = (int) (255 * (1 - cyan) * (1 - black));
+        int g = (int) (255 * (1 - magenta) * (1 - black));
+        int b = (int) (255 * (1 - yellow) * (1 - black));
+        String hex = "#" + Integer.toHexString(r) + Integer.toHexString(g) + Integer.toHexString(b);
+        return hex;
+    }
+
+}
